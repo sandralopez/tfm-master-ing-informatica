f20290b8c16942cbde63787b1535eda6ffee98a6
sandralopez <slopez52751@gmail.com>
2024-08-06 22:20:48 -0400

    Se añade código para aplicar el modelo de clasificación de dígitos MNIST a una imagen

---
+++ models/mnist-test.py
@@ -0,0 +1,23 @@
+#%%
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+
+#%%
+# Cargar el modelo
+model = tf.keras.models.load_model(os.getenv('MODEL_PATH'))
+
+#%%
+# Cargar y preprocesar la imagen
+img_path = os.getenv('IMAGE_PATH')
+img_width, img_height = 28, 28
+img = image.load_img(img_path, color_mode='grayscale', target_size=(img_width, img_height))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array /= 255.0
+
+#%%
+# Hacer la predicción
+prediction = model.predict(img_array)
+print(np.argmax(prediction))
