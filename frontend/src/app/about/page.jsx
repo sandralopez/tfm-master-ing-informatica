7146cc98fdf136a544bcab3c846e477530e6b85d
sandralopez <slopez52751@gmail.com>
2024-09-17 14:47:50 -0300

    Se modifica el texto de la página Acerca de

--- frontend/src/app/about/page.jsx
+++ frontend/src/app/about/page.jsx
@@ -3,12 +3,13 @@ import { Description } from "../../components/shared/Description";
 export default function About() {
   return (
     <main>
-      <Description header="Acerca de Explicabilidad CNN" subheader="Proyecto Fin de Máster en Ingeniería Informática" />
+      <Description header="Acerca de Explicabilidad CNN" subheader="Explicabilidad aplicada a modelos de redes neuronales convolucionales" />
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <p>Explicabilidad CNN: Clasificación de imágenes y explicabilidad</p>
-        <p>Este proyecto Fin de Máster consiste ofrecer una forma fácil para realizar clasificación de imágenes utilizando modelos de redes neuronales convolucionales a los que aplicar librerías de interpretabilidad con el objetivo de explicar los resultados obtenidos.</p>
-        <p>A través de esta página puedes seleccionar entre algunos modelos de clasificación ya entrenados y librerías de explicabilidad para aplicarlos a una imagen. La finalidad es la visualización de resultados de forma sencilla e intuitiva.</p>
-        <p>Este proyecto es mi Trabajo Fin de Máster de Ingeniería Informática en la Universidad de Burgos.</p>
+        <p className="text-xl font-semibold my-4">Explicabilidad CNN: Clasificación de imágenes y explicabilidad</p>
+        <p className="mb-2">Este proyecto es parte de mi Trabajo Final del Máster en Ingeniería Informática en la Universidad de Burgos.</p>
+        <p className="mb-2"> El objetivo es ofrecer una forma sencilla de que los usuarios puedan ejecutar los modelos de redes neuronales disponibles a los que además se les aplica bibliotecas de explicabilidad para hacer comprensibles los resultados obtenidos.</p>
+        <p className="mb-2">Este proyecto consta de varias partes creadas con distintas tecnologías. El frontend fue desarrollado con Next.js y como backend se tienen varios servicios desarrollados con FastApi. Las bibliotecas de explicabilidad incorporadas aplican la función Grad-CAM para generar un mapa de calor que muestre las partes más relevantes de la imagen para el resultado obtenido.</p>
+        <p className="mb-2">La explicabilidad permite hacer más transparente el funcionamiento de los modelos, haciendo más sencilla su evaluación, su comparación o la detección de sesgos.</p>
       </div>
     </main>
   );
